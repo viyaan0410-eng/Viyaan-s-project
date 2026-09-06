66970a249895a3195a2e8da26ae7a6cc8bf1f243
Dance of the fire dragon <viyaan0410@gmail.com>
2026-09-06 04:56:06 +0000

    Problem 1 in project 1

--- Project 1.py
+++ Project 1.py
@@ -1,1 +1,18 @@
-print ('Hello World')
+# Title 
+
+print ('DDDDD'   ,'IIIII','  A  ','RRRRR','Y   Y',sep='  ')
+print ('D    D'  ,'  I  ','A   A','R   R',' YYY ',sep='  ')
+print ('D     D' ,'  I  ','AAAAA','RRRRR','  Y  ',sep='  ')
+print ('D    D'  ,'  I  ','A   A','R  R ','  Y  ',sep='  ')
+print ('DDDDD'   ,'IIIII','A   A','R   R','  Y  ',sep='  ')
+
+# Ask
+
+player = input ('What is your name: ')
+intrest = input ('\v What is your intrest: ')
+school = input ('\v What is your schools name: ')
+Sport = input ('\vWhat is you favorite sport:  ')
+
+# Real Code
+
+print ('Dear diary, today while I was going to school I found a person, his name was', player '\v' 'He had just recently joined', school, 'we both quickly became friends because we both had the same hobby which was', intrest, )
